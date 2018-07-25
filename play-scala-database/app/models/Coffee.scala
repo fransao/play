3e5b76368a929c8ab708740d864133156f3bef7a
fransao <francisco-salazar8a@hotmail.com>
2018-07-25 16:59:48 -0500

    add project

--- play-scala-database/app/models/Coffee.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-import play.api.libs.json.Json
-
-case class Coffee(name: String, price: Double, farm: String)
-
-object Coffee {
-  implicit val cofffeFormat = Json.format[Coffee]
-}
